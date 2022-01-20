a759e77d8f53b411be03b7d0eeae64ccf9411079
siyeon <sylee@megazone.com>
2022-01-20 20:21:39 +0900

    feat: _document 파일 추가 및 tailwind 테스트 컴포넌트 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import tw from 'twin.macro';
+
+const TestComponent = tw.span`text-red-500 text-3xl`;
 
 const Home: NextPage = () => {
   return (
@@ -16,9 +19,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <h1 className="text-3xl font-bold underline text-red-500">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <TestComponent>Test</TestComponent>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
