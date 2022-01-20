8ea473cfa9fb315328cac96d6aac34045f91fb9b
siyeon <sylee@megazone.com>
2022-01-20 21:03:09 +0900

    feat: react query 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
-import { FC } from 'react';
-import Head from 'next/head';
-import { GlobalStyles } from 'twin.macro';
+import React, { useState } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>Fancy Mouse</title>
-    </Head>
-    <GlobalStyles />
-    <Component {...pageProps} />
-  </>
-);
+function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>Fancy Mouse</title>
+        </Head>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
