1beb683e00ca380ae4b3dea75ee9876945b03915
siyeon <sylee@megazone.com>
2022-01-29 21:21:45 +0900

    feat: global 스타일 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,32 @@ import Head from 'next/head';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import '../styles/globals.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 960px;
+  min-height: 100vh;
+  position: relative;
+  margin: 2rem auto 0;
+`;
 
 function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Head>
-          <title>팬시마우스</title>
-        </Head>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>팬시마우스</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
 
