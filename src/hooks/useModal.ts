1b06a814e3eb0ee66aa82152150c8f4991eca6af
siyeon <sylee@megazone.com>
2022-02-01 20:50:53 +0900

    feat: modal 훅 추가

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { MODAL_TYPE, ModalType } from '@/components/modals/type';
+
+const useModal = () => {
+  const [visible, setVisible] = useState(false);
+  const [modalType, setModalType] = useState<ModalType | undefined>(MODAL_TYPE.WORD);
+
+  const handleModal = (type: ModalType) => {
+    setVisible(!visible);
+    setModalType(type);
+  };
+
+  return { visible, handleModal, modalType };
+};
+
+export default useModal;
