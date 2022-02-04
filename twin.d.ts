9e4389e752d6e50a28321e6b261c16472f3f5da6
siyeon <sylee@megazone.com>
2022-02-05 00:46:54 +0900

    feat: css prop 사용 위해 tsconfig 수정

--- twin.d.ts
+++ twin.d.ts
@@ -1,8 +1,27 @@
 import 'twin.macro';
 import styledImport from '@emotion/styled';
 import { css as cssImport } from '@emotion/react';
+import { DOMAttributes } from 'react';
 
 declare module 'twin.macro' {
   const styled: typeof styledImport;
   const css: typeof cssImport;
+}
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string;
+    }
+  }
 }
