696d5150e31fcdb23e8b67968d31dd9b783b09ae
siyeon <sylee@megazone.com>
2022-02-06 11:20:52 +0900

    style: colorset 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff',
+      primaryWeak: '#5D6166',
+      primary: '#35393d',
+      primaryDark: '#191B1E',
+      gray: {
+        10: '#fdfdfd',
+        20: '#f7f8f9',
+        30: '#e9ebee',
+        40: '#c5c8ce',
+        50: '#666F7B',
+        60: '#3A4552',
+        70: '#404B5A',
+        80: '#161D24',
+        90: '#17202D',
+      },
+    },
   },
   plugins: [],
 };
