008a362764557763314326e3c0b14ddceafb8139
siyeon <sylee@megazone.com>
2022-02-13 02:07:24 +0900

    feat: 폴더 모달 내 컬러 select/hover/default 기반 렌더링

---
+++ src/components/modals/constants.ts
@@ -0,0 +1,50 @@
+export const FOLDER_COLORS = [
+  {
+    folderId: 'folder01',
+    color: '#3FC7DB',
+  },
+  {
+    folderId: 'folder02',
+    color: '#72C0FD',
+  },
+  {
+    folderId: 'folder03',
+    color: '#91A6FC',
+  },
+  {
+    folderId: 'folder04',
+    color: '#B196FD',
+  },
+  {
+    folderId: 'folder05',
+    color: '#E499F6',
+  },
+  {
+    folderId: 'folder06',
+    color: '#F5A5C2',
+  },
+  {
+    folderId: 'folder07',
+    color: '#FC8888',
+  },
+  {
+    folderId: 'folder08',
+    color: '#FFC177',
+  },
+  {
+    folderId: 'folder09',
+    color: '#FFE475',
+  },
+  {
+    folderId: 'folder10',
+    color: '#BEDB84',
+  },
+  {
+    folderId: 'folder11',
+    color: '#74DAA9',
+  },
+  {
+    folderId: 'folder00',
+    color: '#BFCDDE',
+  },
+];
