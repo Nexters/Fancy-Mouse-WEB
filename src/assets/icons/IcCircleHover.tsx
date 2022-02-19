0cbd14b6d71d90039823263dc80c273c8ebb1c14
siyeon <sylee@megazone.com>
2022-02-19 23:00:15 +0900

    fix: icons

--- src/assets/icons/IcCircleHover.tsx
+++ src/assets/icons/IcCircleHover.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
 
-const SvgIcCircleHover = (props: SVGProps<SVGSVGElement>) => (
+interface Props extends SVGProps<SVGSVGElement> {
+  color: string;
+}
+
+const SvgIcCircleHover = (props: Props) => (
   <svg width={40} height={40} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <circle opacity={0.4} cx={20} cy={20} r={20} fill="#BFCDDE" />
+    <circle opacity={0.4} cx={20} cy={20} r={20} fill={props.color} />
     <circle cx={20} cy={20} r={17} fill="#fff" />
-    <circle cx={20} cy={20} r={16} fill="#BFCDDE" />
+    <circle cx={20} cy={20} r={16} fill={props.color} />
   </svg>
 );
 
