d2a4cf26bca0019a7f9139eecbcdef7115affba8
dev-owen <jgyuity1289@gmail.com>
2022-02-25 02:37:47 +0900

    fix: 이미지 처리 방식 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
 };
 
 module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
